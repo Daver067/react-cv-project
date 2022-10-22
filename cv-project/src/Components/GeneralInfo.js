30a93306a9f21c82f9173627da8b81ddd1b732c8
Dave <davediemert_88@hotmail.com>
2022-10-22 12:40:57 -0700

    finished boilerplate of resume builder

---
+++ cv-project/src/Components/GeneralInfo.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class GeneralInfo extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+    render() {
+        return()
+    }
+}
+
+export default GeneralInfo;
